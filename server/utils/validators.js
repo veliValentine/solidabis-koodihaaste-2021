89b6a2e2bec2d738f7f6117d8a499acebaab8ce6
veliValentine <nicolas.valentine@helsinki.fi>
2021-05-25 21:23:24 +0300

    feat: validator helpers

--- server/utils/validators.js
+++ server/utils/validators.js
@@ -4,6 +4,20 @@ const isValidNumber = (number) => {
   return true;
 };
 
+const argumentsAreValidNumbers = (...inputs) => {
+  if (inputs[0] !== inputs.filter(filterFalsyValues).length - 1) {
+    throw new Error('Missing arguments');
+  }
+  const invalidArguments = inputs.filter(filterInvalidValues).length;
+  if (invalidArguments) {
+    throw new TypeError('Argument not a number');
+  }
+};
+
+const filterFalsyValues = (value) => value;
+const filterInvalidValues = (value) => !isValidNumber(value);
+
 module.exports = {
   isValidNumber,
+  argumentsAreValidNumbers,
 };
