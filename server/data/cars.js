c45db7d3a27fad14ade62f02cd76dd8823885f58
veliValentine <nicolas.valentine@helsinki.fi>
2021-05-25 20:07:38 +0300

    chore: mock data

---
+++ server/data/cars.js
@@ -0,0 +1,16 @@
+const cars = [
+  {
+    id: 'A',
+    consumption: 3,
+  },
+  {
+    id: 'B',
+    consumption: 3.5,
+  },
+  {
+    id: 'C',
+    consumption: 4,
+  },
+];
+
+module.exports = cars;
