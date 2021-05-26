85a239d5f1f8c093d4ed21348587b3009ab1d761
veliValentine <nicolas.valentine@helsinki.fi>
2021-05-26 22:15:19 +0300

    feat: car service

---
+++ client/src/services/carService.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const CARS_API_URL = '/api/v1/cars';
+
+export const cars = async () => {
+  try {
+    const { data } = await axios.get(CARS_API_URL);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const consumption = async (vehicle, distance, velocity) => {
+  try {
+    const { data } = await axios.get(`${CARS_API_URL}/${vehicle}/${distance}/${velocity}`);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const compare = async (vehicle, distance, velocity1, velocity2) => {
+  try {
+    const { data } = await axios.get(`${CARS_API_URL}/${vehicle}/${distance}/${velocity1}/${velocity2}`);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
