9547acb37d46e52688760c471008de156c06f02c
veliValentine <nikkevanukas1995@gmail.com>
2021-06-01 12:02:25 +0300

    refactor: data hook

--- client/src/hooks/useData.jsx
+++ client/src/hooks/useData.jsx
@@ -1,30 +1,37 @@
 import { useState } from 'react';
-import { compare, consumption } from '../services/carService';
+import carService from '../services/carService';
 
 const useData = () => {
   const [data1, setData1] = useState(null);
   const [data2, setData2] = useState(null);
   const [difference, setDifference] = useState(null);
 
-  const updateData = (values) => {
+  const updateData = async (values = {}) => {
     const { vehicle, distance, velocity1, velocity2 } = values;
+    if (!vehicle || !distance) return;
     if (velocity2) {
-      compare(vehicle, distance, velocity1, velocity2)
-        .then(setData);
-    } else {
-      consumption(vehicle, distance, velocity1)
-        .then(setData);
+      return setData(await carService.compare(vehicle, distance, velocity1, velocity2));
     }
+    if (velocity1) {
+      return setData(await carService.consumption(vehicle, distance, velocity1));
+    }
+    return setData();
   };
 
-  const setData = (data) => {
+  const setData = (data = {}) => {
     const { data1 = null, data2 = null, difference = null } = data;
     setData1(data1);
     setData2(data2);
     setDifference(difference);
   };
 
-  return [data1, data2, difference, updateData];
+  const data = {
+    data1,
+    data2,
+    difference
+  };
+
+  return [data, updateData];
 };
 
 export default useData;
