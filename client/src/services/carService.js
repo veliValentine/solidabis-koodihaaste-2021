5499e5365974ab64d704532a1741a21b7d9a7708
veliValentine <nikkevanukas1995@gmail.com>
2021-06-04 10:25:25 +0300

    refactor: support server changes

--- client/src/services/carService.js
+++ client/src/services/carService.js
@@ -14,11 +14,7 @@ export const cars = async () => {
 export const consumption = async (vehicle, distance, velocity) => {
   try {
     const { data } = await axios.get(`${CARS_API_URL}/${vehicle}/${distance}/${velocity}`);
-
-    return {
-      ...data,
-      data1: data
-    };
+    return data;
   } catch (error) {
     console.error(error);
   }
@@ -27,11 +23,7 @@ export const consumption = async (vehicle, distance, velocity) => {
 export const compare = async (vehicle, distance, velocity1, velocity2) => {
   try {
     const { data } = await axios.get(`${CARS_API_URL}/${vehicle}/${distance}/${velocity1}/${velocity2}`);
-    return {
-      ...data,
-      data1: data.data_1 ? data.data_1 : data.data1,
-      data2: data.data_2 ? data.data_2 : data.data2,
-    };
+    return data;
   } catch (error) {
     console.error(error);
   }
